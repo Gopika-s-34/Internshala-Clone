4d2210be6fe401d466812a8be6e7915d40384c95
Gopiks-s-34 <sivagopika34@gmail.com>
2025-07-30 21:39:20 +0530

    Fix: update backend package.json start script for Render deployment

--- backend/Routes/internship.js
+++ backend/Routes/internship.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Internship = require("../Model/internship");
+const Internship = require("../Model/Internship");
 
 router.post("/", async (req, res) => {
     const Internshipdata = new Internship({
